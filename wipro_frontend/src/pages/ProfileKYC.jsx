0bde0eeac42e1cc6787e59f3a9c15799aa81db92
subhan2108 <sk1623969@gamil.com>
2026-01-19 22:12:39 +0530

    borhgsdbfkjk

--- wipro_frontend/src/pages/ProfileKYC.jsx
+++ wipro_frontend/src/pages/ProfileKYC.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiFetch } from "../api/api";
-import "../styles/profilekyc.css";
+import "../styles/ProfileKYC.css";
 
 export default function KYC() {
   const navigate = useNavigate();
